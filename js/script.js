948ec9fecaace8d7303e6c8ada5e247a628034d8
SK0RPIA <contact@skorpia.fr>
2023-08-12 18:12:57 +0200

    feat: :sparkles: Add unit choice for W/kW and display the results

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 function calculateCosts() {
-    var watts = document.getElementById('watts').value;
+    var unit = document.getElementById('unit').value;
+    var power = document.getElementById('power').value;
+    var hours = document.getElementById('hours').value;
     var costPerKWh = document.getElementById('cost').value;
-    var dailyCost = (watts / 1000) * costPerKWh * 24;
-    var monthlyCost = dailyCost * 30;
-    var yearlyCost = dailyCost * 365;
 
-    document.getElementById('daily').textContent = dailyCost.toFixed(2);
-    document.getElementById('monthly').textContent = monthlyCost.toFixed(2);
-    document.getElementById('yearly').textContent = yearlyCost.toFixed(2);
+    // Convertir en kilowatts si l'unité choisie est en watts
+    if (unit === 'W') {
+        power = power / 1000;
+    }
+
+    var dailyCost = power * hours * costPerKWh;
+    var monthlyCost = dailyCost * 30; // Approximation pour un mois
+    var yearlyCost = dailyCost * 365; // Calcul pour une année
+
+    document.getElementById('dailyCost').textContent = dailyCost.toFixed(2);
+    document.getElementById('monthlyCost').textContent = monthlyCost.toFixed(2);
+    document.getElementById('yearlyCost').textContent = yearlyCost.toFixed(2);
 
     document.getElementById('results').style.display = 'block';
 }
