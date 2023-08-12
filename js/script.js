c1b01606ee2dfdd1ff2eecc1ad48727d788c5e0e
SK0RPIA <contact@skorpia.fr>
2023-08-12 18:49:31 +0200

    feat: :sparkles: Addition of a selector for the number of hours of use

--- js/script.js
+++ js/script.js
@@ -3,14 +3,30 @@ function calculateCosts() {
     var costUnit = document.getElementById('costUnit').value;
     var power = document.getElementById('power').value;
     var costPerKWh = document.getElementById('cost').value;
-    var hours = document.getElementById('hours').value;
+    var hours = document.getElementById('hoursInput').value;
+    var hoursUnit = document.getElementById('hoursUnit').value;
 
     if (unit === 'W') {
-        power = power / 1000; // Convertir en kilowatts
+        power = power / 1000;
     }
 
     if (costUnit === 'cent') {
-        costPerKWh = costPerKWh / 100; // Convertir en euros
+        costPerKWh = costPerKWh / 100;
+    }
+
+    switch (hoursUnit) {
+        case 'day':
+            hours = hours * 1;
+            break;
+        case 'week':
+            hours = hours * 7;
+            break;
+        case 'month':
+            hours = hours * 30;
+            break;
+        case 'year':
+            hours = hours * 365;
+            break;
     }
 
     var dailyCost = power * hours * costPerKWh;
